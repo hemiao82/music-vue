dbdde182737d577752c0bdea6a86366357c3189b
yuan <76913289@qq.com>
2022-06-02 23:02:26 +0800

    修改路径映射配置

--- src/api/hello.ts
+++ src/api/hello.ts
@@ -1,5 +1,5 @@
 import request from  "./request"
-export function holle(params:object){
+export function holle(params?:object){
   return  request({
         url: "/holle",
         method: 'get',
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const path = require('path')
+const path=require("path")
 // https://vitejs.dev/config/
 export default
 	defineConfig({
@@ -12,12 +12,12 @@ export default
 		plugins: [
 			vue()
 		],
+
 		resolve: {
 			alias: {
-				'@': path.join(__dirname, 'src'),
-				'#': path.join(__dirname, 'types')
+			  "@": path.resolve(__dirname, "./src"),
 			}
-		}
+		  }
 		// resolve: {
 		// 	alias: {
 		// 		'@': '/src'
